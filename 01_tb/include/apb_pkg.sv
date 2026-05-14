73004bb37502c6b5a46b1e3a551d011b9d91f580
vnguyen <vnguyen@verifasttech.com>
2026-05-14 21:12:14 +0530

    Fix all syntax errros but can't get config

--- 01_tb/include/apb_pkg.sv
+++ 01_tb/include/apb_pkg.sv
@@ -5,19 +5,18 @@ package apb_pkg;
     import uvm_pkg::*;
     `include "uvm_macros.svh"
 
-    // Include tất cả các file theo thứ tự
-    `include "interface/apb_if.sv"
-    `include "env/apb_agent/apb_transaction.sv"
-    `include "env/apb_agent/apb_driver.sv"
-    `include "env/apb_agent/apb_monitor.sv"
-    `include "env/apb_agent/apb_agent.sv"
-    `include "env/apb_scoreboard.sv"
-    `include "env/apb_env.sv"
-    `include "seq/apb_base_seq.sv"
-    `include "seq/apb_write_seq.sv"
-    `include "seq/apb_read_seq.sv"
-    `include "test/apb_base_test.sv"
+    // CHỈ include các file CLASS (UVM Components)
+    // Thứ tự include cực kỳ quan trọng: Cái nào dùng trước include trước
+    `include "apb_transaction.sv"
+    `include "apb_driver.sv"
+    `include "apb_monitor.sv"
+    `include "apb_agent.sv"
+    `include "apb_scoreboard.sv"
+    `include "apb_env.sv"
+    `include "apb_base_seq.sv"
+    `include "apb_write_seq.sv"
+    `include "apb_read_seq.sv"
+    `include "apb_base_test.sv"
 
 endpackage
-
 `endif
