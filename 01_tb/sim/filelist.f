b178b318158b52a7530e0772d6126be2a19c7f14
vnguyen <vnguyen@verifasttech.com>
2026-05-14 15:30:13 +0530

    huhu

---
+++ 01_tb/sim/filelist.f
@@ -0,0 +1,11 @@
+// filelist.f
++incdir+../interface
++incdir+../include
++incdir+../env/apb_agent
++incdir+../env
++incdir+../seq
++incdir+../test
+
+../interface/apb_if.sv
+../include/apb_pkg.sv    // <--- Chỉ biên dịch package này
+../tb/tb_top.sv
